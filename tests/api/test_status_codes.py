80432d477d0bac7f9b49d230da8e5a92be1a37ad
Anastasiia Demenkova <anastasiia.demenkova@gmail.com>
2025-11-21 15:53:47 -0800

    Add UI, API, and smoke markers

--- tests/api/test_status_codes.py
+++ tests/api/test_status_codes.py
@@ -1,7 +1,10 @@
+import pytest
 import requests
 from utils.env_loader import load_environment, get_base_url
 
-def test_homepage_status_code():
+@pytest.mark.api
+def test_status_code_is_200():
     load_environment("preprod")
     response = requests.get(get_base_url())
     assert response.status_code == 200
+
--- tests/ui/test_homepage.py
+++ tests/ui/test_homepage.py
@@ -1,6 +1,10 @@
+import pytest
 from utils.env_loader import load_environment, get_base_url
 
+@pytest.mark.ui
+@pytest.mark.smoke
 def test_homepage_title(page):
     load_environment("preprod")
     page.goto(get_base_url())
     assert "Polyester" in page.title()
+
